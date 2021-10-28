e765c86250ebb9c3e084fd762c5f6a18e635713b
jtorrelles <jhonnattan.torrelles@gmail.com>
2021-10-28 12:15:42 -0300

    initial commit

---
+++ src/main/java/com/mindata/es/superheroeschallenge/MinDataChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.mindata.es.superheroeschallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinDataChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinDataChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mindata/es/superheroeschallenge/MinDataChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindata.es.superheroeschallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinDataChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
