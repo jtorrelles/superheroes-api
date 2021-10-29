01f717ebb2fac6ae4ee2dc94c4e09750a9e61d4b
jtorrelles <jhonnattan.torrelles@gmail.com>
2021-10-29 01:07:22 -0300

    se agregan test al service

---
+++ src/test/java/com/mindata/es/superheroeschallenge/SuperHeroesServiceTest.java
@@ -0,0 +1,55 @@
+package com.mindata.es.superheroeschallenge;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.mindata.es.superheroeschallenge.dto.SuperHeroesDto;
+import com.mindata.es.superheroeschallenge.exceptions.SuperHeroesNotFoundException;
+import com.mindata.es.superheroeschallenge.models.SuperHeroe;
+import com.mindata.es.superheroeschallenge.repository.SuperHeroesRepository;
+import com.mindata.es.superheroeschallenge.services.SuperHeroesService;
+import com.mindata.es.superheroeschallenge.services.impl.SuperHeroesServiceImpl;
+
+@ExtendWith(MockitoExtension.class)
+public class SuperHeroesServiceTest {
+
+	@Mock
+	private SuperHeroesRepository superHeroesRepository;
+	
+	private SuperHeroesService superHeroeService;
+
+	@BeforeEach
+	public void setUp() throws Exception{
+		superHeroeService = new SuperHeroesServiceImpl(superHeroesRepository);
+	}
+
+	@Test
+	public void findAll_returnSuperHeroesInfo() {
+		when(superHeroesRepository.findAll()).thenReturn(new ArrayList<SuperHeroe>() {
+			{
+				add(new SuperHeroe(1L, "Superman"));
+				add(new SuperHeroe(2L, "Batman"));
+			}
+		});
+
+		List<SuperHeroesDto> result = superHeroeService.getAllSuperHeroes();
+
+		assertThat(result.size()).isGreaterThan(0);
+	}
+	
+	@Test
+	public void findAll_returnNotFound() {
+		when(superHeroesRepository.findAll()).thenReturn(new ArrayList<SuperHeroe>());
+		assertThrows(SuperHeroesNotFoundException.class, () -> superHeroeService.getAllSuperHeroes());
+	}
+}
