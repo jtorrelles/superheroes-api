20f4f6c0448b0ef6f4de718878f3d5d671232dee
jtorrelles <jhonnattan.torrelles@gmail.com>
2021-10-28 23:26:27 -0300

    se agrega service layer

---
+++ src/main/java/com/mindata/es/superheroeschallenge/services/SuperHeroesService.java
@@ -0,0 +1,9 @@
+package com.mindata.es.superheroeschallenge.services;
+
+import java.util.List;
+
+import com.mindata.es.superheroeschallenge.dto.SuperHeroesDto;
+
+public interface SuperHeroesService {
+	List<SuperHeroesDto> getAllSuperHeroes();
+}
---
+++ src/main/java/com/mindata/es/superheroeschallenge/services/impl/SuperHeroesServiceImpl.java
@@ -0,0 +1,18 @@
+package com.mindata.es.superheroeschallenge.services.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.mindata.es.superheroeschallenge.dto.SuperHeroesDto;
+import com.mindata.es.superheroeschallenge.services.SuperHeroesService;
+
+@Service
+public class SuperHeroesServiceImpl implements SuperHeroesService{
+
+	@Override
+	public List<SuperHeroesDto> getAllSuperHeroes() {
+		return null;
+	}
+
+}
