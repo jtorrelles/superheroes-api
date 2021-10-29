6a8ec89c3ae4400b2a3febc04d3691d3201a166a
jtorrelles <jhonnattan.torrelles@gmail.com>
2021-10-29 16:53:46 -0300

    se agrega configuracion basica de flyway, se incluyen sql de estructura y datos

---
+++ src/main/resources/db/migration/V1__Create_Table_SuperHeroe.sql
@@ -0,0 +1,2 @@
+CREATE TABLE super_heroes(id bigint auto_increment, name varchar(255));
+ALTER TABLE super_heroes ADD PRIMARY KEY (id);
---
+++ src/main/resources/db/migration/V2__Populate_SuperHeroe_Table.sql
@@ -0,0 +1,4 @@
+INSERT INTO super_heroes(name) VALUES('Superman');
+INSERT INTO super_heroes(name) VALUES('Batman');
+INSERT INTO super_heroes(name) VALUES('Flash');
+INSERT INTO super_heroes(name) VALUES('Wonder Woman');
