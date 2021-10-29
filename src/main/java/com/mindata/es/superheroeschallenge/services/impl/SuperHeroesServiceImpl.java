033e2f92b8dabcc21e4231cde8c7d34a7dfaaafc
jtorrelles <jhonnattan.torrelles@gmail.com>
2021-10-29 17:40:01 -0300

    se implementa la funcionalidad en el servicio

--- src/main/java/com/mindata/es/superheroeschallenge/services/impl/SuperHeroesServiceImpl.java
+++ src/main/java/com/mindata/es/superheroeschallenge/services/impl/SuperHeroesServiceImpl.java
@@ -1,25 +1,35 @@
 package com.mindata.es.superheroeschallenge.services.impl;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.StreamSupport;
 
 import org.springframework.stereotype.Service;
 
 import com.mindata.es.superheroeschallenge.dto.SuperHeroesDto;
+import com.mindata.es.superheroeschallenge.exceptions.SuperHeroesNoContentException;
+import com.mindata.es.superheroeschallenge.models.SuperHeroe;
 import com.mindata.es.superheroeschallenge.repository.SuperHeroesRepository;
 import com.mindata.es.superheroeschallenge.services.SuperHeroesService;
 
 @Service
-public class SuperHeroesServiceImpl implements SuperHeroesService{
+public class SuperHeroesServiceImpl implements SuperHeroesService {
 
 	private SuperHeroesRepository superHeroesRepository;
-	
+
 	public SuperHeroesServiceImpl(SuperHeroesRepository superHeroesRepository) {
 		this.superHeroesRepository = superHeroesRepository;
 	}
-	
+
 	@Override
 	public List<SuperHeroesDto> getAllSuperHeroes() {
-		return null;
+		List<SuperHeroesDto> result = new ArrayList<>();
+		Iterable<SuperHeroe> response = superHeroesRepository.findAll();
+		if (StreamSupport.stream(response.spliterator(), false).count() == 0) {
+			throw new SuperHeroesNoContentException();
+		}
+		response.forEach(superHeroe -> result.add(new SuperHeroesDto(superHeroe.getId(), superHeroe.getName())));
+		return result;
 	}
 
 }
