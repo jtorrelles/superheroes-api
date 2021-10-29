ec7202c6675e2517dc5e4c877b39e5cd6933747c
jtorrelles <jhonnattan.torrelles@gmail.com>
2021-10-29 01:06:59 -0300

    se inyecta la dependencia del rpoitory en el service

--- src/main/java/com/mindata/es/superheroeschallenge/services/impl/SuperHeroesServiceImpl.java
+++ src/main/java/com/mindata/es/superheroeschallenge/services/impl/SuperHeroesServiceImpl.java
@@ -5,11 +5,18 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.mindata.es.superheroeschallenge.dto.SuperHeroesDto;
+import com.mindata.es.superheroeschallenge.repository.SuperHeroesRepository;
 import com.mindata.es.superheroeschallenge.services.SuperHeroesService;
 
 @Service
 public class SuperHeroesServiceImpl implements SuperHeroesService{
 
+	private SuperHeroesRepository superHeroesRepository;
+	
+	public SuperHeroesServiceImpl(SuperHeroesRepository superHeroesRepository) {
+		this.superHeroesRepository = superHeroesRepository;
+	}
+	
 	@Override
 	public List<SuperHeroesDto> getAllSuperHeroes() {
 		return null;
