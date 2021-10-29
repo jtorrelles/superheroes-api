9293889a76a5c63096affe9c4cdfb24a8fa073d2
jtorrelles <jhonnattan.torrelles@gmail.com>
2021-10-29 19:28:50 -0300

    se eliminan imports sin usar

--- src/main/java/com/mindata/es/superheroeschallenge/controllers/SuperHeroesController.java
+++ src/main/java/com/mindata/es/superheroeschallenge/controllers/SuperHeroesController.java
@@ -2,10 +2,8 @@ package com.mindata.es.superheroeschallenge.controllers;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mindata.es.superheroeschallenge.dto.SuperHeroesDto;
